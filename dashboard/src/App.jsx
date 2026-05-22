3583d0210fd92c356b7c2c55307921c6bc8bc76a
kurniadi2271 <pratamakurniadi@gmail.com>
2026-05-22 19:36:59 +0700

    change code App.jsx to cloud

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,29 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [stats, setStats] = useState({
-    total: 0,
-  });
-
+  const [stats, setStats] = useState({ total: 0 });
   const [users, setUsers] = useState([]);
 
-  // pindahkan ke atas
-  const fetchData = async () => {
-    const statsRes = await axios.get(
-      "http://localhost:3000/stats"
-    );
+  const API = import.meta.env.VITE_API_URL;
 
-    const usersRes = await axios.get(
-      "http://localhost:3000/users"
-    );
-
-    setStats(statsRes.data);
-    setUsers(usersRes.data);
-  };
 
   useEffect(() => {
     const load = async () => {
-      await fetchData();
+      try {
+        const statsRes = await axios.get(`${API}/stats`);
+        const usersRes = await axios.get(`${API}/users`);
+
+        setStats(statsRes.data);
+        setUsers(usersRes.data);
+      } catch (err) {
+        console.log(err.message);
+      }
     };
 
     load();
@@ -38,40 +32,25 @@ function App() {
 
       <div className="bg-green-500 text-white p-5 rounded-xl w-64">
         <h2>Total Subscriber</h2>
-
-        <p className="text-4xl">
-          {stats.total}
-        </p>
+        <p className="text-4xl">{stats.total}</p>
       </div>
 
       <div className="mt-10">
-        <h2 className="text-2xl mb-3">
-          User List
-        </h2>
+        <h2 className="text-2xl mb-3">User List</h2>
 
         <table className="border w-full">
           <thead>
             <tr>
-              <th className="border p-2">
-                ID
-              </th>
-
-              <th className="border p-2">
-                Chat ID
-              </th>
+              <th className="border p-2">ID</th>
+              <th className="border p-2">Chat ID</th>
             </tr>
           </thead>
 
           <tbody>
             {users.map((u) => (
               <tr key={u.id}>
-                <td className="border p-2">
-                  {u.id}
-                </td>
-
-                <td className="border p-2">
-                  {u.chat_id}
-                </td>
+                <td className="border p-2">{u.id}</td>
+                <td className="border p-2">{u.chat_id}</td>
               </tr>
             ))}
           </tbody>
